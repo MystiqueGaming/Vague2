88f595529d0a33af1a6c4a89d25a9ecafedd86b6
Kevin David <kevin.d.alappat@gmail.com>
2018-11-23 11:41:39 +0530

    added product card and full img card removed form card

--- vague2.0-material-template/v2-md_js.js
+++ vague2.0-material-template/v2-md_js.js
@@ -1,49 +1,53 @@
 function validateForm() {
-    var name = document.forms["vmd-form"]["name"];
-    var email = document.forms["vmd-form"]["email"];
-    var pass = document.forms["vmd-form"]["password"];
-    var cpass = document.forms["vmd-form"]["c-password"];
+    var name = document.querySelector('.name-input');
+    var email = document.querySelector('.email-input');
+    var pass = document.querySelector('.pass-input');
+    var cpass = document.querySelector('.cpass-input');
 
     if (name.value == "") {
-        document.getElementById('name-input').style.borderBottom = '2px solid #d32f2f';
-        document.getElementById('name-error-msg').innerHTML = 'Please Enter A Name';
+        document.querySelector('.name-input').style.borderBottom = '2px solid #d32f2f';
+        document.querySelector('.name-error-msg').innerHTML = 'Please Enter A Name';
         return false;
     }
     else {
-        document.getElementById('name-input').style.borderBottom = '2px solid #bdbdbd';
-        document.getElementById('name-error-msg').innerHTML = '';
+        document.querySelector('.name-input').style.borderBottom = '2px solid #bdbdbd';
+        document.querySelector('.name-error-msg').innerHTML = '';
     }
 
     if (email.value == "") {
-        document.getElementById('email-input').style.borderBottom = '2px solid #d32f2f';
-        document.getElementById('email-error-msg').innerHTML = 'Please Enter A Email';
+        document.querySelector('.email-input').style.borderBottom = '2px solid #d32f2f';
+        document.querySelector('.email-error-msg').innerHTML = 'Please Enter A Email';
         return false;
     }
     else if (email.value.indexOf("@", 0) < 0)                 
     { 
-        document.getElementById('email-input').style.borderBottom = '2px solid #d32f2f';
-        document.getElementById('email-error-msg').innerHTML = 'Please Enter A Valid Email';
+        document.querySelector('.email-input').style.borderBottom = '2px solid #d32f2f';
+        document.querySelector('.email-error-msg').innerHTML = 'Please Enter A Valid Email';
         return false; 
     } 
    
     else if (email.value.indexOf(".", 0) < 0)                 
     { 
-        document.getElementById('email-input').style.borderBottom = '2px solid #d32f2f';
-        document.getElementById('email-error-msg').innerHTML = 'Please Enter A Valid Email'; 
+        document.querySelector('.email-input').style.borderBottom = '2px solid #d32f2f';
+        document.querySelector('.email-error-msg').innerHTML = 'Please Enter A Valid Email'; 
         return false; 
     }
     else {
-        document.getElementById('email-input').style.borderBottom = '2px solid #bdbdbd';
-        document.getElementById('email-error-msg').innerHTML = '';
+        document.querySelector('.email-input').style.borderBottom = '2px solid #bdbdbd';
+        document.querySelector('.email-error-msg').innerHTML = '';
     } 
     if (pass.value == "") {
-        document.getElementById('pass-input').style.borderBottom = '2px solid #d32f2f';
-        document.getElementById('pass-error-msg').innerHTML = 'Please Enter A Password';
+        document.querySelector('.pass-input').style.borderBottom = '2px solid #d32f2f';
+        document.querySelector('.pass-error-msg').innerHTML = 'Please Enter A Password';
         return false;
     }
+    else {
+        document.querySelector('.pass-input').style.borderBottom = '2px solid #bdbdbd';
+        document.querySelector('.pass-error-msg').innerHTML = '';
+    }
     if (cpass.value != pass.value) {
-        document.getElementById('cpass-input').style.borderBottom = '2px solid #d32f2f';
-        document.getElementById('cpass-error-msg').innerHTML = 'Passwords Do Not Match';
+        document.querySelector('.cpass-input').style.borderBottom = '2px solid #d32f2f';
+        document.querySelector('.cpass-error-msg').innerHTML = 'Passwords Do Not Match';
         return false;
     }
     return true;
