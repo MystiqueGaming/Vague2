799321c88438b151d17a209beea48a27501d8016
Kevin David <kevin.d.alappat@gmail.com>
2018-11-25 10:20:35 +0530

    v2_ver-beta2.2
    Cleaned up folder, added comments , added liscence

--- vague2.0-material-template/JavaScript/v2-md_js.js
+++ vague2.0-material-template/JavaScript/v2-md_js.js
@@ -1,9 +1,28 @@
-function validateForm() {
-    var name = document.querySelector('.name-input');
-    var email = document.querySelector('.email-input');
-    var pass = document.querySelector('.pass-input');
-    var cpass = document.querySelector('.cpass-input');
 
+//    Copyright [2018] [Kevin David / Mystique Gaming]
+
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+
+//        http://www.apache.org/licenses/LICENSE-2.0
+
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+
+
+// Validation For Forms
+function validateForm() {
+    var name = document.querySelector('.name-input');               // Name Input Variable
+    var email = document.querySelector('.email-input');             // Email Input Variable
+    var pass = document.querySelector('.pass-input');               // Password Input Variable
+    var cpass = document.querySelector('.cpass-input');             // Confirm Password Input Variable
+
+    // Checks wether name input is empty and sets styles respectively
     if (name.value == "") {
         document.querySelector('.name-input').style.borderBottom = '2px solid #d32f2f';
         document.querySelector('.name-error-msg').innerHTML = 'Please Enter A Name';
@@ -14,11 +33,14 @@ function validateForm() {
         document.querySelector('.name-error-msg').innerHTML = '';
     }
 
+    // Checks wether email input is empty and sets styles respectively
     if (email.value == "") {
         document.querySelector('.email-input').style.borderBottom = '2px solid #d32f2f';
         document.querySelector('.email-error-msg').innerHTML = 'Please Enter A Email';
         return false;
     }
+
+    // Checks wether email input contains an @ sign and sets styles respectively
     else if (email.value.indexOf("@", 0) < 0)                 
     { 
         document.querySelector('.email-input').style.borderBottom = '2px solid #d32f2f';
@@ -26,6 +48,7 @@ function validateForm() {
         return false; 
     } 
    
+    // Checks wether email input contains '.' and sets styles respectively
     else if (email.value.indexOf(".", 0) < 0)                 
     { 
         document.querySelector('.email-input').style.borderBottom = '2px solid #d32f2f';
@@ -36,6 +59,8 @@ function validateForm() {
         document.querySelector('.email-input').style.borderBottom = '2px solid #bdbdbd';
         document.querySelector('.email-error-msg').innerHTML = '';
     } 
+
+    // Checks wether password input is empty and sets styles respectively
     if (pass.value == "") {
         document.querySelector('.pass-input').style.borderBottom = '2px solid #d32f2f';
         document.querySelector('.pass-error-msg').innerHTML = 'Please Enter A Password';
@@ -45,6 +70,8 @@ function validateForm() {
         document.querySelector('.pass-input').style.borderBottom = '2px solid #bdbdbd';
         document.querySelector('.pass-error-msg').innerHTML = '';
     }
+
+    // Checks wether confirm password input is equal to password input and sets styles respectively
     if (cpass.value != pass.value) {
         document.querySelector('.cpass-input').style.borderBottom = '2px solid #d32f2f';
         document.querySelector('.cpass-error-msg').innerHTML = 'Passwords Do Not Match';
